bc4d239626bb3ca94085fe1e4eaf21a8f36d9a95
shankargundrelead <59326715+shankargundrelead@users.noreply.github.com>
2021-08-20 19:52:45 +0530

    Update SpringbootPathlockApplication.java

--- src/main/java/com/glt/spring/path/SpringbootPathlockApplication.java
+++ src/main/java/com/glt/spring/path/SpringbootPathlockApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @RestController
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SpringbootPathlockApplication {
