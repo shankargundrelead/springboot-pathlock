37386f8989e2f541cd49e4be42ff689e056ad132
shankargundrelead <59326715+shankargundrelead@users.noreply.github.com>
2021-08-20 19:42:10 +0530

    Update SpringbootPathlockApplicationTests.java

--- src/test/java/com/glt/spring/path/SpringbootPathlockApplicationTests.java
+++ src/test/java/com/glt/spring/path/SpringbootPathlockApplicationTests.java
@@ -3,7 +3,7 @@ package com.glt.spring.path;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootTest
 class SpringbootPathlockApplicationTests {
 
 	@Test
